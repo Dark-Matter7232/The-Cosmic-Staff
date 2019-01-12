756caeb6b92f8d9226d9d0ad7894dcfa7c430dde
kdrag0n <dragon@khronodragon.com>
2019-01-12 15:39:15 -0800

    sched/tune: introduce direct set_stune_boost()
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: mcdachpappe <noreference@web.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1981,6 +1981,7 @@ extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
+int set_stune_boost(char *st_name, int boost, int *boost_default);
 #else /* !CONFIG_DYNAMIC_STUNE_BOOST */
 static inline int do_stune_boost(char *st_name, int boost, int *slot)
 {
@@ -1993,6 +1994,11 @@ static inline int do_stune_sched_boost(char *st_name, int *slot)
 }
 
 static inline int reset_stune_boost(char *st_name, int slot)
+{
+	return 0;
+}
+
+static inline int set_stune_boost(char *st_name, int boost, int *boost_default)
 {
 	return 0;
 }
