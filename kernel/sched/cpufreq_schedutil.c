ea7b00c1a6eaf5b367865334bf3c50321800ee6a
0ctobot <return.of.octobot@gmail.com>
2019-11-03 19:12:43 -0500

    cpufreq: schedutil: Expose default configuration options and apply init protection
    Adapted from https://github.com/kdrag0n/proton_zf6@d9faa70:
    
    Allow governor tunables to be configured in-kernel and guard
    against userspace interference.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -10,6 +10,7 @@
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#include <linux/binfmts.h>
 
 #include <linux/cpufreq.h>
 #include <linux/kthread.h>
@@ -596,6 +597,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -615,6 +619,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
