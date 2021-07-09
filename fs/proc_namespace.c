b5eb5139d6426bd76e4f171b02ce776789fcbb17
AnuragRai <kerneldeveloper7232@gmail.com>
2021-07-09 09:36:49 +0000

    Revert "fs/proc: hide magisk mounts for IsolatedService"
    This reverts commit 1a47628dfffa084201c14d836e3f0b401831ef4b.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -94,21 +94,6 @@ static void show_type(struct seq_file *m, struct super_block *sb)
 	}
 }
 
-static inline int skip_magisk_entry(const char *devname)
-{
-#ifdef CONFIG_PROC_MAGISK_HIDE_MOUNT
-	if (devname && strstr(devname, "magisk")) {
-		char name[TASK_COMM_LEN];
-		get_task_comm(name, current);
-		if (strstr(name, "Binder") ||
-		    strstr(name, "JavaBridge")) {
-			return SEQ_SKIP;
-		}
-	}
-#endif
-	return 0;
-}
-
 static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 {
 	struct proc_mounts *p = m->private;
@@ -122,9 +107,6 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 		if (err)
 			goto out;
 	} else {
-		err = skip_magisk_entry(r->mnt_devname);
-		if (err)
-			goto out;
 		mangle(m, r->mnt_devname ? r->mnt_devname : "none");
 	}
 	seq_putc(m, ' ');
@@ -197,9 +179,6 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 		if (err)
 			goto out;
 	} else {
-		err = skip_magisk_entry(r->mnt_devname);
-		if (err)
-			goto out;
 		mangle(m, r->mnt_devname ? r->mnt_devname : "none");
 	}
 	seq_puts(m, sb_rdonly(sb) ? " ro" : " rw");
@@ -231,10 +210,6 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 			goto out;
 	} else {
 		if (r->mnt_devname) {
-			err = skip_magisk_entry(r->mnt_devname);
-			if (err)
-				goto out;
-
 			seq_puts(m, "device ");
 			mangle(m, r->mnt_devname);
 		} else
