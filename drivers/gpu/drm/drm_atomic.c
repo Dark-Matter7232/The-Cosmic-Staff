aac036f96338a8227b5b85de054dcf922c640709
AnuragRai <S10C-4484@vidyavihar.org>
2021-07-17 22:07:16 +0530

    Update drm_atomic.c

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2249,7 +2249,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 		return -EINVAL;
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
-		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		devfreq_boost_kick(DEVFREQ_EXYNOS_MIF);
 
 	drm_modeset_acquire_init(&ctx, 0);
 
