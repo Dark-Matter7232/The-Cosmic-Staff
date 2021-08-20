e97aae4783bc4c87643f3196866826e7810bcd7e
John Vincent <git@tensevntysevn.cf>
2021-08-20 21:00:17 +0800

    kernel: Affine Samsung's Wi-Fi daemon and HAL to the big CPU cluster
    SCSC's WLBT driver is a latency-sensitive service that needs all the lowest latency it needs, as it crashes when it waits for the response for too long between userspace and kernel.
    
    Let's affine the service on the faster big CPU cluster.
    
    Signed-off-by: John Vincent <git@tensevntysevn.cf>

--- fs/exec.c
+++ fs/exec.c
@@ -90,6 +90,8 @@ static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
+#define SEC_WLBTD_BIN_PREFIX "/vendor/bin/wlbtd"
+#define SEC_WLAN_HAL_BIN_PREFIX "/vendor/bin/hw/vendor.samsung.hardware.wifi"
 #define SFLINGER_BIN_PREFIX "/system/bin/surfaceflinger"
 
 
@@ -1956,6 +1958,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 		if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		} else if (unlikely(!strncmp(filename->name, SEC_WLBTD_BIN_PREFIX, strlen(SEC_WLBTD_BIN_PREFIX))) ||
+				   unlikely(!strncmp(filename->name, SEC_WLAN_HAL_BIN_PREFIX, strlen(SEC_WLAN_HAL_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
